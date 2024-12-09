27f502fd99763480ea621b58785e0d0ccfa31781
paulleadbeater1987 <paul_leadbeater@live.co.uk>
2024-12-09 15:06:06 +0000

    config changes for live hosting

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
     define('ROOT_URL', 'http://localhost/phpsandbox/dataMySQLi/');
 
     define ('DB_HOST', 'localhost');
-    define ('DB_USER', 'root');
-    define ('DB_PASS', 'h0meLessNess!');
-    define ('DB_NAME', 'pottaseodium');
+    define ('DB_USER', 'englnigd_seodium_user');
+    define ('DB_PASS', 'H4jHPtDu@UQp8cb');
+    define ('DB_NAME', 'englnigd_seodium');
